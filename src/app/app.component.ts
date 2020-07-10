a4e2053bdba69cbf3ce1dcb0ad976693a6dafe67
Ashutosh Sharma <email2ashusharma@gmail.com>
2020-07-11 01:46:04 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  guessForm = new FormGroup({});
+
+  disableGuessFlag = false;
   title = 'guess-game';
+  noOfTries = 0;
+  gameNumber: number;
+  guessNumber: number;
+  deviation: number;
+  message: string;
+  state: string;
+
+
+  ngOnInit(): void {
+    this.initializeGame();
+  }
+
+
+  guess(): void {
+    this.noOfTries++;
+    this.deviation = this.guessNumber - this.gameNumber;
+    if (this.deviation === 0) {
+      this.message = 'Perfect!! This is the same number I thought! It took ' + this.noOfTries + ' guesses. ';
+      this.state = 'success';
+      this.disableGuessFlag = true;
+    } else if (this.deviation < 0) {
+      this.message = ' Your guess ' + this.guessNumber + ' is smaller';
+      this.state = 'danger';
+    } else {
+      this.message = ' Your guess ' + this.guessNumber + ' is higher';
+      this.state = 'danger';
+    }
+  }
+
+
+  initializeGame(): void {
+    this.noOfTries = 0;
+    this.gameNumber = Math.trunc(Math.random() * 100) + 1;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
